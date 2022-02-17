ba488c9df2a01a69ba018ad9457a88828917e6d8
cyberian <fox4080@gmail.com>
2022-02-17 19:14:51 +0700

    Добавлен сваггер

---
+++ src/main/java/com/shop/myshopforcft/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.shop.myshopforcft.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ivan Vinnichenko
+ */
+@Api
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @ApiOperation("проверка работы")
+    @PostMapping("/test")
+    public void deletePost(){
+    }
+}
